bd9a9521b57278003a852fecebac7eae4e208bcc
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-12-02 15:38:57 +0000

    Use kitty as shell

--- tools/afl/fuzz.sh
+++ tools/afl/fuzz.sh
@@ -17,7 +17,7 @@ test -e ./tools/afl/$1_testcases
 test -e ./tools/afl/$1_runner.janet
 
 echo "running fuzz master..."
-xterm -e \
+kitty sh -c \
   "afl-fuzz -i ./tools/afl/$1_testcases -o ./fuzz_out/$1 -M Fuzz$1_0 -- ./build/janet ./tools/afl/$1_runner.janet @@" &
 children="$! $children"
 echo "waiting for afl to get started before starting secondary fuzzers"
@@ -27,7 +27,7 @@ NFUZZ=$((NFUZZ - 1))
 
 for N in $(seq $NFUZZ)
 do
-  xterm -e \
+  kitty sh -c \
     "afl-fuzz -i ./tools/afl/$1_testcases -o ./fuzz_out/$1 -S Fuzz$1_$N -- ./build/janet ./tools/afl/$1_runner.janet @@" &
   children="$! $children"
 done
